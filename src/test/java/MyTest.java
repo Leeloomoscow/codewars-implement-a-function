46978193b1bf3a0f6cdb8908add3f0a1a769c6be
Aleksandra Rudenko <rudenkoay8@gmail.com>
2021-10-03 17:18:48 +0300

    Done

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,8 +1,20 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MyTest {
     @Test
-    void name() {
+    public void sampleTest() {
+        assertEquals(4, K.multiply(2, 2));
+        assertEquals(10, K.multiply(5, 2));
+        assertEquals(100, K.multiply(100, 1));
+        assertEquals(0, K.multiply(0, 1000));
+    }
 
+    private static class K {
+        public static int multiply(int num1, int num2) {
+            int sum = num1 * num2;
+            return sum;
+        }
     }
 }
